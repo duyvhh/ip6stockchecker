516d065c5d6a63a5f7f3a81d93e9d319742a028d
Steve Van <vanhuynhduy@gmail.com>
2014-09-14 14:14:32 +0700

    updated checker.py for placeholder email

--- checker.py
+++ checker.py
@@ -21,7 +21,7 @@ class CheckerHandler(webapp2.RequestHandler):
 
         if not "unavailable" in option['shippingLead'] or option['isBuyable']:
             mail.send_mail(sender="<sender>",
-                           to="<sender>",
+                           to="<recipient>",
                            subject="iPhone 6 is now available in Singapore store. Act now!!!",
                            body="Act now.")
 
