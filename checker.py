3dd085374e7875e0990f6ea129de066c346c8002
Steve Van <stevevan@hoiio.com>
2014-09-14 14:04:27 +0700

    initial commit

---
+++ checker.py
@@ -0,0 +1,28 @@
+import webapp2
+from google.appengine.api import mail
+import logging
+import urllib2
+import json
+
+
+class CheckerHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.info("Checker is being invoked")
+
+        logging.debug("Requesting remote URL")
+        response = urllib2.urlopen('http://store.apple.com/sg/buyFlowSelectionSummary/IPHONE6?node=home/'
+                                   'shop_iphone/family/iphone6&step=select&option.dimensionScreensize=4_7inch&'
+                                   'option.dimensionColor=gold&option.dimensionCapacity=16gb&'
+                                   'option.carrierModel=UNLOCKED%2FWW&carrierPolicyType=UNLOCKED')
+
+        logging.debug("Parsing json")
+        result = json.load(response)
+        option = result['body']['content']['selected']['purchaseOptions']
+
+        if not "unavailable" in option['shippingLead'] or option['isBuyable']:
+            mail.send_mail(sender="<sender>",
+                           to="<sender>",
+                           subject="iPhone 6 is now available in Singapore store. Act now!!!",
+                           body="Act now.")
+
+        self.response.write('OK')
